d1c094ac507ce75f7ea8aa7e3b73345378aad858
julianosfreitas <julianof29contato@gmail.com>
2024-08-29 18:00:18 -0300

    Adicionando Global Styles e ajustando o CSS

--- src/App.js
+++ src/App.js
@@ -107,8 +107,91 @@
 
 // =====================================================================================
 
+// import React, { useState, useEffect } from 'react';
+// import './App.css';
+
+// const App = () => {
+//   const [currentIndex, setCurrentIndex] = useState(0);
+//   const images = [
+//     'img/Imagem 1.jpg',
+//     'img/Imagem 2.jpg',
+//     'img/Imagem 3.jpg',
+//     'img/Imagem 4.jpg',
+//   ];
+
+//   const updateCarousel = () => {
+//     const width = document.querySelector('.carousel-images img').clientWidth;
+//     document.querySelector('.carousel-images').style.transform = `translateX(-${width * currentIndex}px)`;
+//   };
+
+//   const nextImage = () => {
+//     setCurrentIndex(currentIndex < images.length - 1 ? currentIndex + 1 : 0);
+//   };
+
+//   const prevImage = () => {
+//     setCurrentIndex(currentIndex > 0 ? currentIndex - 1 : images.length - 1);
+//   };
+
+//   useEffect(() => {
+//     updateCarousel();
+//     window.addEventListener('resize', updateCarousel);
+//     return () => {
+//       window.removeEventListener('resize', updateCarousel);
+//     };
+//   }, [currentIndex]);
+
+//   return (
+//     <div>
+//       <header>
+//         <div className="logo-container"></div>
+//         <nav>
+//           <ul>
+//             <li><a href="index.html">Início</a></li>
+//             <li><a href="servicos.html">Serviços</a></li>
+//             <li><a href="sobre-nos.html">Sobre Nós</a></li>
+//             <li><a href="#agendamento">Agendamento</a></li>
+//           </ul>
+//         </nav>
+//       </header>
+
+//       <main>
+//         <section id="galeria">
+//           <h2>Galeria</h2>
+//           <div className="carousel">
+//             <button className="prev" onClick={prevImage}>&#10094;</button>
+//             <div className="carousel-images">
+//               {images.map((src, index) => (
+//                 <img key={index} src={src} alt={`Imagem ${index + 1}`} className={index === currentIndex ? 'active' : ''} />
+//               ))}
+//             </div>
+//             <button className="next" onClick={nextImage}>&#10095;</button>
+//           </div>
+//         </section>
+
+//         <section id="agendamento">
+//           <h2>Agendamento</h2>
+//           <div className="embed-container" data-slug="cardetail" data-url="https://www.gendoou.com/"></div>
+//           <script type="text/javascript" src="https://www.gendoou.com/assets/embed/embed.js"></script>
+//           <a href="https://www.gendoou.com/cardetail"><button type="submit">Agendar</button></a>
+//         </section>
+//       </main>
+
+//       <footer>
+//         <p>&copy; 2024 Car Detail Estética Automotiva. Todos os direitos reservados.</p>
+//       </footer>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// ===================================================================================================================================
+
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import './App.css'; // Você pode remover ou ajustar se estiver usando estilos CSS puros
+// import styled, { createGlobalStyle } from 'styled-components';
+// import GlobalStyle from './GlobalStyles'; // Importa o GlobalStyle correto
+import { GlobalStyle, ResponsiveMedia } from './styles/GlobalStyles';
 
 const App = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -141,45 +224,48 @@ const App = () => {
   }, [currentIndex]);
 
   return (
-    <div>
-      <header>
-        <div className="logo-container"></div>
-        <nav>
-          <ul>
-            <li><a href="index.html">Início</a></li>
-            <li><a href="servicos.html">Serviços</a></li>
-            <li><a href="sobre-nos.html">Sobre Nós</a></li>
-            <li><a href="#agendamento">Agendamento</a></li>
-          </ul>
-        </nav>
-      </header>
+    <>
+      <GlobalStyle />
+      <div>
+        <header>
+          <div className="logo-container"></div>
+          <nav>
+            <ul>
+              <li><a href="index.html">Início</a></li>
+              <li><a href="servicos.html">Serviços</a></li>
+              <li><a href="sobre-nos.html">Sobre Nós</a></li>
+              <li><a href="#agendamento">Agendamento</a></li>
+            </ul>
+          </nav>
+        </header>
 
-      <main>
-        <section id="galeria">
-          <h2>Galeria</h2>
-          <div className="carousel">
-            <button className="prev" onClick={prevImage}>&#10094;</button>
-            <div className="carousel-images">
-              {images.map((src, index) => (
-                <img key={index} src={src} alt={`Imagem ${index + 1}`} className={index === currentIndex ? 'active' : ''} />
-              ))}
+        <main>
+          <section id="galeria">
+            <h2>Galeria</h2>
+            <div className="carousel">
+              <button className="prev" onClick={prevImage}>&#10094;</button>
+              <div className="carousel-images">
+                {images.map((src, index) => (
+                  <img key={index} src={src} alt={`Imagem ${index + 1}`} className={index === currentIndex ? 'active' : ''} />
+                ))}
+              </div>
+              <button className="next" onClick={nextImage}>&#10095;</button>
             </div>
-            <button className="next" onClick={nextImage}>&#10095;</button>
-          </div>
-        </section>
+          </section>
 
-        <section id="agendamento">
-          <h2>Agendamento</h2>
-          <div className="embed-container" data-slug="cardetail" data-url="https://www.gendoou.com/"></div>
-          <script type="text/javascript" src="https://www.gendoou.com/assets/embed/embed.js"></script>
-          <a href="https://www.gendoou.com/cardetail"><button type="submit">Agendar</button></a>
-        </section>
-      </main>
+          <section id="agendamento">
+            <h2>Agendamento</h2>
+            <div className="embed-container" data-slug="cardetail" data-url="https://www.gendoou.com/"></div>
+            <script type="text/javascript" src="https://www.gendoou.com/assets/embed/embed.js"></script>
+            <a href="https://www.gendoou.com/cardetail"><button type="submit">Agendar</button></a>
+          </section>
+        </main>
 
-      <footer>
-        <p>&copy; 2024 Car Detail Estética Automotiva. Todos os direitos reservados.</p>
-      </footer>
-    </div>
+        <footer>
+          <p>&copy; 2024 Car Detail Estética Automotiva. Todos os direitos reservados.</p>
+        </footer>
+      </div>
+    </>
   );
 };
 
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,45 +1,6 @@
-.App {
-  text-align: center;
-} */
+import styled, { createGlobalStyle } from 'styled-components';
 
-.App-logo {
-  height: 40vmin;
-  pointer-events: none;
-}
-
-@media (prefers-reduced-motion: no-preference) {
-  .App-logo {
-    animation: App-logo-spin infinite 20s linear;
-  }
-}
-
-.App-header {
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-}
-
-.App-link {
-  color: #61dafb;
-}
-
-@keyframes App-logo-spin {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-}
-
-/* ================================================================================ */
-
-/* Estilos Globais
+// Estilos Globais
 export const GlobalStyle = createGlobalStyle`
   body {
     font-family: Arial, sans-serif;
@@ -326,4 +287,4 @@ export const ServiceItemHeading3 = styled.h3`
 
 export const ServiceItemParagraph = styled.p`
   color: #333;
-`; */
+`;
